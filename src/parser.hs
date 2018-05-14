f0cc917c53a144fd6134672b86463b8fa8c10f11
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2018-05-14 22:12:10 +0200

    refactor parser

--- src/parser.hs
+++ src/parser.hs
@@ -20,37 +20,78 @@ adventureFile =
         eof
         return (GameData locations current backpack flags)
 
+
+--locations
+getLocations :: GenParser Char st (Map LocationId Location)
+getLocations = do
+    getString "locations:["
+    locations <- sepBy getLocation (getString ",")
+    getString "]"
+    return (fromList locations)
+
+getLocation ::GenParser Char st (LocationId, Location)
+getLocation = do
+    getString "("
+    id <- getWord
+    getString ","
+    body <-getLocationBody
+    getString ")"
+    return (id,body)
+    
+
+getLocationBody :: GenParser Char st Location
+getLocationBody = do
+    getString "("
+    description <- getInfo
+    getString ","
+    moves <- getMovesMap
+    objects <- getObjectsMap
+    getString ","
+    getString ","
+    flags <- getFlagList
+    return (Location description moves objects flags)
+
+getMovesMap :: GenParser Char st (Map Direction LocationId )
+getMovesMap = do
+    getString "["
+    locations <- sepBy getMove (getString ",")
+    getString "]"
+    return (fromList locations)
+        
+getMove ::GenParser Char st (Direction, LocationId)
+getMove = do
+    getString "("
+    direction <- getWord
+    getString ","
+    locationId <- getWord
+    getString ")"
+    return (direction, locationId)
+
+
+--current
 getCurrent :: GenParser Char st LocationId
 getCurrent = do 
     getString "current:"
     result <-  getWord
     return result
-    
-getWord :: GenParser Char st String
-getWord = many alphaNum 
 
-getInfo ::  GenParser Char st String
-getInfo = do
-    string "\""
-    content <- many (noneOf "\"") 
-    string "\""
-    return ("\"" ++ content ++ "\"")
 
-getFlags :: GenParser Char st [String]
-getFlags = do 
-    getString "flags:["
-    result <- getFlagList
-    getString "]"
-    return result
-
-getFlagList :: GenParser Char st [String]
-getFlagList = sepBy getWord (getString ",")
-
+--backpack
 getBackpack :: GenParser Char st (Map ObjectId Object) 
 getBackpack = do
-    getString "backpack:["
+    getString "backpack:"
     getObjectsMap
 
+
+--flags
+getFlags :: GenParser Char st [String]
+getFlags = do 
+    getString "flags:"
+    result <- getFlagList
+    return result
+    
+
+--object helpers
 getObjectsMap :: GenParser Char st (Map ObjectId Object) 
 getObjectsMap = do
     getString "["
@@ -82,6 +123,12 @@ getObjectBody = do
 getAction :: GenParser Char st String
 getAction = return ""
 
+
+--helpers
+
+getWord :: GenParser Char st String
+getWord = many (alphaNum <|> (char '_')) 
+
 getString :: String-> GenParser Char st String
 getString [] = return ""
 getString (c:cs) = do
@@ -91,50 +138,12 @@ getString (c:cs) = do
     tail <- getString cs
     return (c:tail)
 
-getLocations :: GenParser Char st (Map LocationId Location)
-getLocations = do
-    getString "locations:["
-    locations <- sepBy getLocation (getString ",")
-    getString "]"
-    return (fromList locations)
+getInfo ::  GenParser Char st String
+getInfo = do
+    string "\""
+    content <- many (noneOf "\"") 
+    string "\""
+    return ("\"" ++ content ++ "\"")
 
-getLocation ::GenParser Char st (LocationId, Location)
-getLocation = do
-    getString "("
-    id <- getWord
-    getString ","
-    body <-getLocationBody
-    getString ")"
-    return (id,body)
-
-
-getLocationBody :: GenParser Char st Location
-getLocationBody = do
-    getString "("
-    description <- getInfo
-    getString ","
-    moves <- getMovesMap
-    objects <- getObjectsMap
-    getString ","
-    getString ","
-    flags <- getFlagList
-    return (Location description moves objects flags)
-
-    
-getMovesMap :: GenParser Char st (Map Direction LocationId )
-getMovesMap = do
-    getString "["
-    locations <- sepBy getMove (getString ",")
-    getString "]"
-    return (fromList locations)
-        
-getMove ::GenParser Char st (Direction, LocationId)
-getMove = do
-    getString "("
-    direction <- getWord
-    getString ","
-    locationId <- getWord
-    getString ")"
-    return (direction, locationId)
-
-
+getFlagList :: GenParser Char st [String]
+getFlagList = sepBy getWord (getString ",")
