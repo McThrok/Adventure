aa49b0163f7e187d17e78b41cfa3f3e6c062b476
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2018-05-18 19:49:08 +0200

    add DeriveGeneric

--- src/dataModel.hs
+++ src/dataModel.hs
@@ -1,13 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
 module DataModel
 where
 
+import GHC.Generics
 import Control.Monad.Trans.State
 import Data.List
 import Data.Set
 import Data.Map.Lazy hiding (foldl,map)
 
 
-data Command = Go | Take | Use | Look | Interact | Inventory | Save | Load | New | Quit | Help deriving Show
+data Command = Go | Take | Use | Look | Interact | Inventory | Save | Load | New | Quit | Help deriving (Generic, Show)
 
 type Direction = String
 type ObjectId = String
@@ -22,14 +24,14 @@ data Object = Object{
     interact::ActionId,
     use::ActionId,
     objectFlags::Set Flag -- includes canBeTaken
-} deriving Show
+} deriving (Generic, Show)
 
 data Location = Location{
     description::String,
     moves::Map Direction LocationId,
     objects::Map ObjectId Object,
     locationFlags::Set Flag
-} deriving Show
+} deriving (Generic, Show)
 
 data GameData = GameData{
     locations::Map LocationId Location,
@@ -38,13 +40,13 @@ data GameData = GameData{
     interActions::Map ActionId Action,
     useActions::Map ActionId (ObjectId, Action),
     gameFlags::Set Flag -- include Won and Lost
-} deriving Show
+} deriving (Generic, Show)
 
 type GameStateT a = StateT GameData IO a
 
 type Action = [Instruction]
-data Instruction = Print String | Change [String] ChangeType ChangeValue | IfStatement Exp Action deriving Show
-data ChangeType = Add | Delete | Assign deriving Show
-data ChangeValue =  StringValue String | ObjectValue Object | LocationValue Location deriving Show
-data Exp = Leaf [String] | Not Exp | And Exp Exp | Or Exp Exp deriving Show
+data Instruction = Print String | Change [String] ChangeType ChangeValue | IfStatement Exp Action deriving (Generic, Show)
+data ChangeType = Add | Delete | Assign deriving (Generic, Show)
+data ChangeValue =  StringValue String | ObjectValue Object | LocationValue Location deriving (Generic, Show)
+data Exp = Leaf [String] | Not Exp | And Exp Exp | Or Exp Exp deriving (Generic, Show)
 
