cdf7a0e6fa78b54e65ea469f3c8ccd32914786d1
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2018-06-07 23:40:51 +0200

    fix flags

--- src/changeActionInterpreter.hs
+++ src/changeActionInterpreter.hs
@@ -13,7 +13,7 @@ evalChange :: [String] -> ChangeType -> ChangeValue -> GameStateT ()
 evalChange ("locations":tail) change value = modify (\s -> s {locations = (evalLocations tail change value (locations s))})
 evalChange ["current"] Assign (StringValue value) = modify (\s -> s {current = value})
 evalChange ("backpack":tail) change value = modify (\s -> s {backpack = (evalObjects tail change value (backpack s))})
-evalChange ["flags", flag] change (StringValue value) = modify (\s -> s {gameFlags = (evalFlags change value (gameFlags s))})
+evalChange ["flags"] change (StringValue value) = modify (\s -> s {gameFlags = (evalFlags change value (gameFlags s))})
 evalChange _ _ _ = return ()
 
 evalObjects ::[String] -> ChangeType -> ChangeValue -> Map ObjectId Object -> Map ObjectId Object
@@ -26,7 +26,7 @@ evalObject :: [String] -> ChangeType -> String -> Object -> Object
 evalObject ["objectInfo"] Assign value obj = obj{objectInfo = value}
 evalObject ["interAction"] Assign value obj = obj{interAction = value}
 evalObject ["useAction"] Assign value obj = obj{useAction = value}
-evalObject ["flags",flag] change value obj = obj{objectFlags = evalFlags change value (objectFlags obj)}
+evalObject ["flags"] change value obj = obj{objectFlags = evalFlags change value (objectFlags obj)}
 evalObject _ _ _ obj = obj
 
 evalLocations ::[String] -> ChangeType -> ChangeValue -> Map LocationId Location -> Map LocationId Location
@@ -40,7 +40,7 @@ evalLocation ["locationInfo"] Assign (StringValue value) loc = loc{locationInfo 
 evalLocation ["moves"] Delete (StringValue value) loc = loc{moves = delete value (moves loc)}
 evalLocation ["moves",id] Assign (StringValue value) loc = loc{moves = insert id value (moves loc)}
 evalLocation ("objects":tail) change value loc = loc{objects = (evalObjects tail change value (objects loc))}
-evalLocation ["flags",flag] change (StringValue value) loc = loc{locationFlags = evalFlags change value (locationFlags loc)}
+evalLocation ["flags"] change (StringValue value) loc = loc{locationFlags = evalFlags change value (locationFlags loc)}
 evalLocation _ _ _ l = l
 
 evalFlags :: ChangeType -> String -> S.Set Flag -> S.Set Flag
